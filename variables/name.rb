dc67746c3abd3970d50425a3bab69e2d31324ad4
Mila Hose <mila.hose@gmail.com>
2017-01-22 18:09:59 -0800

    add greeting 10 times

--- variables/name.rb
+++ variables/name.rb
@@ -1,3 +1,12 @@
+# Ask the user to type in their name and greet them with a personalalized message. 
+
 puts "Hello, what is your name?"
 name = gets.chomp.downcase
 puts "Hi #{name.capitalize}, welcome to Ruby programming!"
+
+
+# Add another sectio that prints out the name of the user 10 times
+greeting = "Hi #{name.capitalize}, welcome to Ruby programming!"
+10.times do 
+	puts greeting 
+end 
